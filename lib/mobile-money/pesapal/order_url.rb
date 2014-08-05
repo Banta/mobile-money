1d3455cb3b1fc0791250b3aee0946d54bfc138d5
Bernard Banta <banta.bernard@gmail.com>
2014-08-05 14:58:30 +0300

    Used ERB::Util.url_encode instead of CGI::escape

--- lib/mobile-money/pesapal/order_url.rb
+++ lib/mobile-money/pesapal/order_url.rb
@@ -63,7 +63,7 @@ module Pesapal
     def params_to_string
       strings = []
       params.each do |key, value|
-        strings << "#{key}=#{CGI::escape(value)}"
+        strings << "#{key}=#{ERB::Util.url_encode(value)}"
       end
       strings.join('&')
     end
